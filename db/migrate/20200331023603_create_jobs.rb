8449b708f3c0c069aaa06908cd111e785f50bda5
CaioFML <caio.felipemotta@hotmail.com>
2020-03-30 23:36:18 -0300

    cria migration Job

---
+++ db/migrate/20200331023603_create_jobs.rb
@@ -0,0 +1,15 @@
+class CreateJobs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.text :description
+      t.integer :contract
+      t.string :job_link
+      t.integer :salary
+      t.string :company_name
+      t.date :published_date
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_022606) do
+ActiveRecord::Schema.define(version: 2020_03_31_023603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "contract"
+    t.string "job_link"
+    t.integer "salary"
+    t.string "company_name"
+    t.date "published_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
