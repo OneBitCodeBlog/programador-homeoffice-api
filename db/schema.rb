b6c12919e06556411f41c7d6cfda92387fe1608b
CaioFML <caio.felipemotta@hotmail.com>
2020-03-30 23:41:42 -0300

    gera migration search

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_023902) do
+ActiveRecord::Schema.define(version: 2020_03_31_024104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,10 +42,22 @@ ActiveRecord::Schema.define(version: 2020_03_31_023902) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "searches", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "alarm_rate"
+    t.bigint "job_key_word_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["job_key_word_id"], name: "index_searches_on_job_key_word_id"
+    t.index ["user_id"], name: "index_searches_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "searches", "job_key_words"
+  add_foreign_key "searches", "users"
 end
