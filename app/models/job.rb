eb50d715a3ab62b81ca6d188a71f594b4203285b
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2020-04-01 00:15:27 -0300

    Adiciona enums para contract em Job (#3)
    * adiciona enums para contract
    
    * adiciona enum para caso seja um ou outro

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,4 +4,11 @@ class Job < ApplicationRecord
 
   has_many :job_key_words, dependent: :destroy
   has_many :key_words, through: :job_key_words
+
+  enum contract: {
+    not_specified: 0,
+    CLT: 1,
+    PJ: 2,
+    CLT_or_PJ: 3
+  }
 end
