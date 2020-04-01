f14d39a1cdbeb34f17efc15f5953f8c8e0c8d0a9
CaioFML <caio.felipemotta@hotmail.com>
2020-03-31 23:22:50 -0300

    configura shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,14 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
