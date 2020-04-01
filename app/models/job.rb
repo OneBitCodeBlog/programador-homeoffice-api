4d5d9126b7ed75e3d1c7709abf9e06964be06597
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2020-03-31 23:58:55 -0300

    Adiciona testes, validações e associações do model Job (#2)
    * configura shoulda matchers
    
    * Cria teste do model Job
    
    * Adiciona validações e relacionamentos ao model Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,7 @@
 class Job < ApplicationRecord
+  validates_presence_of :title, :description,
+                        :job_link, :published_date
+
+  has_many :job_key_words, dependent: :destroy
+  has_many :key_words, through: :job_key_words
 end
