cd66af9dbcbb8920052c5ae54b0b29775e888863
Andre Oliveira <desmonplays@gmail.com>
2020-04-01 01:38:42 -0400

    (new feature) adding and testing enums of contracts

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -12,4 +12,11 @@ RSpec.describe Job do
     it { is_expected.to have_many(:job_key_words).dependent(:destroy) }
     it { is_expected.to have_many(:key_words).through(:job_key_words) }
   end
+
+  describe 'when have enums' do
+    it { expect(Job.new(contract: 0)).to be_not_specified }
+    it { expect(Job.new(contract: 1)).to be_clt }
+    it { expect(Job.new(contract: 2)).to be_pj }
+    it { expect(Job.new(contract: 3)).to be_clt_or_pj }
+  end
 end
