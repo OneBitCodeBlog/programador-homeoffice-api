481671fc900b254fc662e839921c0a3ce548b76d
CaioFML <caio.felipemotta@hotmail.com>
2020-04-02 00:13:53 -0300

    correções

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class Job < ApplicationRecord
-  enum contract: %i[not_specified clt pj clt_or_pj]
+  enum contract: {
+    not_specified: 0,
+    clt: 1,
+    pj: 2,
+    clt_or_pj: 3
+  }
 
   has_many :job_key_words, dependent: :destroy
   has_many :key_words, through: :job_key_words
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Job do
-  describe 'when have validations' do
+  describe "validations" do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to validate_presence_of(:job_link) }
     it { is_expected.to validate_presence_of(:published_date) }
   end
 
-  describe 'when have associations' do
+  describe "associations" do
     it { is_expected.to have_many(:job_key_words).dependent(:destroy) }
     it { is_expected.to have_many(:key_words).through(:job_key_words) }
   end
 
-  describe 'when have enums' do
+  describe "enums" do
     it { expect(Job.new(contract: 0)).to be_not_specified }
     it { expect(Job.new(contract: 1)).to be_clt }
     it { expect(Job.new(contract: 2)).to be_pj }
