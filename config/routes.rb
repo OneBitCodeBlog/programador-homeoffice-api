50e521e196f474b72a27fdf30ca57a18dbc8cc06
Andre Oliveira <desmonplays@gmail.com>
2020-04-02 11:42:50 -0400

    (new feature) installing and configuring sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  mount Sidekiq::Web => '/sidekiq'
 end
