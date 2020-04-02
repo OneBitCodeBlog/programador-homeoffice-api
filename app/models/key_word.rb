a7ada48f7fd5b14450a2c8afe17bfd8a3193353b
CaioFML <caio.felipemotta@hotmail.com>
2020-04-02 00:50:07 -0300

    Adiciona validação, associações e testes para o model Key Word

--- app/models/key_word.rb
+++ app/models/key_word.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class KeyWord < ApplicationRecord
+  validates_presence_of :tag
+
+  has_many :job_key_words, dependent: :destroy
+  has_many :jobs, through: :job_key_words
 end
