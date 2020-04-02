9a7cf78b786cac9ed453886f1badf6ae00db7894
Adriano Avelino <adriano-st777@hotmail.com>
2020-04-02 11:36:45 -0300

    Teste model user (#7)
    adiciona os testes de model para User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :searchs, dependent: :destroy
+  has_many :key_words, through: :searchs
+  validates :name, presence: true
 end
