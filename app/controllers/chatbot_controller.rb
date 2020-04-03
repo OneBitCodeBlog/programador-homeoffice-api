52756ba09c10424b548b122bf007f110b7b3f20e
Luan Solino <luansolino@gmail.com>
2020-04-03 03:20:25 -0300

    service of create user created

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -1,24 +1,14 @@
-class ChatBotController < ApplicationController
+class ChatbotController < ApplicationController
 
   def webhook
+    
     request.body.rewind
-    result = JSON.parse(request.body.read)["queryResult"]
-
-    if result["contexts"].present?
-      response = InterpretService.call(result["action"], result["contexts"][0]["parameters"])
-    else
-      response = InterpretService.call(result["action"], result["parameters"])
-    end
-
-    content_type :json, charset: 'utf-8' 
-    {
-      "fulfillmentText": response,
-      "payload": {
-        "telegram": {
-          "text": response,
-          "parse_mode": "Markdown"
-        }
-      }
-    }.to_json
+    result = JSON.parse(request.body.read)
+    action = result["queryResult"]["action"]
+    
+    response = InterpretService.call(action: action, result: result)
+    
+    render :json => response
+    
   end
 end
--- app/services/interpret_service.rb
+++ app/services/interpret_service.rb
@@ -1,9 +1,18 @@
 class InterpretService
 
-  def self.call(action, params)
+  def self.call(action:, result:)
     case action
     when "search"
-      SearchJobService.new(params).call()
+      keyword = result["queryResult"]["parameters"]["keyword"]
+      SearchJobService.new(keyword: keyword).call()
+    when "alert"
+      session = result["session"].split('/')[-1]
+      language = result["queryResult"]["parameters"]["linguagem"]
+      CreateAlertService.new(session: session, language: language).call
+    when "input.welcome"
+      session = result["session"].split('/')[-1]
+      name = result["queryResult"]["parameters"]["name"]
+      CreateUserService.new(session: session, name: name).call()
     else
       "Desculpe, mas não te entendi. Tente novamente"
     end
