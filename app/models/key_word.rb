cc82269c5f74effe8ed3aafbe4ed75ba468d0ca1
toti.cavalcanti <toticavalcanti@hotmail.com>
2020-04-04 15:39:03 -0300

    set models/key_word

--- app/models/key_word.rb
+++ app/models/key_word.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class KeyWord < ApplicationRecord
-    enum contract: %i[not_specified clt pj clt_or_pj]
   
     has_many :job_key_words, dependent: :destroy
     has_many :key_words, through: :job_key_words
