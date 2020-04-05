d1eb383e977493df75551a05e6c1e1e5ec04cd4d
CaioFML <caio.felipemotta@hotmail.com>
2020-04-05 02:18:56 -0300

    Defini rotas para Jobs

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :job, only: %i[index show]
+      resources :jobs, only: %i[index show]
     end
   end
 end
