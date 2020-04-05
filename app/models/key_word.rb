f0da14b086105788218daa1eb8a43b9c544b769e
toti.cavalcanti <toticavalcanti@hotmail.com>
2020-04-05 01:04:06 -0300

    merge with controller_jobs branch

--- app/models/key_word.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class KeyWord < ApplicationRecord
-  
-    has_many :job_key_words, dependent: :destroy
-    has_many :key_words, through: :job_key_words
-  
-    validates :tag, :created_at, :updated_at, presence: true
-  end
