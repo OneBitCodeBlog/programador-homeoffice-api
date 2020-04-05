1da315096d33d7354952d63ffc1a8d706e110f14
Adriano Avelino <adriano-st777@hotmail.com>
2020-04-05 17:11:54 -0300

    adiciona paginação no controller key_word

--- app/controllers/api/v1/key_words_controller.rb
+++ app/controllers/api/v1/key_words_controller.rb
@@ -2,7 +2,12 @@ module API
   module V1
     class KeyWordsController < ApplicationController
       def index
-        @key_words = KeyWord.all
+        page = params[:page]
+        page_number = page.try(:[], :number)
+        per_page = page.try(:[], :size)
+
+        @key_words = KeyWord.all.page(page_number).per(per_page)
+
         render json: @key_words
       end
 
