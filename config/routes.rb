4ca5a0bc45d94304d81804704def9ac5d61a42a5
Adriano Avelino <adriano-st777@hotmail.com>
2020-04-05 16:44:48 -0300

    adiciona rotes para jey_words

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :jobs, only: %i[index show]
+      resources :key_words, only: %i[index show]
     end
   end
 end
