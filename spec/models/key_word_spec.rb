a2aea46557ea2adcb04d979faaaf08b2c97a6542
CaioFML <caio.felipemotta@hotmail.com>
2020-04-05 00:59:36 -0300

    Adiciona has_many searches e users

--- spec/models/key_word_spec.rb
+++ spec/models/key_word_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe KeyWord do
   describe "associations" do
     it { is_expected.to have_many(:job_key_words).dependent(:destroy) }
     it { is_expected.to have_many(:jobs).through(:job_key_words) }
+    it { is_expected.to have_many(:searches).dependent(:destroy) }
+    it { is_expected.to have_many(:users).through(:searches) }
   end
 
   describe "validations" do
