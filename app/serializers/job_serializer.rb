9ef2f3e2befcef9a4850d31466357d0fe457be56
CaioFML <caio.felipemotta@hotmail.com>
2020-04-05 03:31:30 -0300

    corrige job serializer

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -2,6 +2,5 @@ class JobSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :contract,
              :job_link, :salary, :company_name, :published_date
 
-  has_many :job_key_words
-  has_many :key_words, through: :job_key_words
+  has_many :key_words
 end
