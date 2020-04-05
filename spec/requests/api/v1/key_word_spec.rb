b700051a1fe1354e753e8c3847e4b9bbf0b5ae72
Adriano Avelino <adriano-st777@hotmail.com>
2020-04-05 16:53:33 -0300

    adiciona teste para GET#index em key_words com response with json

--- spec/requests/api/v1/key_word_spec.rb
+++ spec/requests/api/v1/key_word_spec.rb
@@ -7,9 +7,20 @@ RSpec.describe "KeyWords", type: :request do
     end
   end
 
+  let(:parsed_response) { JSON.parse(response.body) }
+  let(:parsed_json_attributes) do
+    parsed_response["data"].first["attributes"].keys
+  end
+  let(:json_attributes) { ["tag"] }
+
   shared_examples "request endpoint" do
     it "responds with 200" do
       expect(response).to have_http_status :ok
+    end
+
+    it "responds with json" do
+      expect(parsed_json_attributes)
+        .to match_array json_attributes
     end
   end
 
