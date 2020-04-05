fd0d45272791965aca8fc4c673b10e689c673608
toti.cavalcanti <toticavalcanti@hotmail.com>
2020-04-05 09:49:42 -0300

    merging chatbot with controller_jobs

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  post '/chatbot', to: 'chatbot#webhook'
-=======
   namespace :api do
     namespace :v1 do
       resources :jobs, only: %i[index show]
     end
   end
->>>>>>> c8d0479a3f74306a0735073fb3315c89a9bc32fc
 end
