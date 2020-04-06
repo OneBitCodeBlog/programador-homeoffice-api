9197226fe9d3a524c0caff9514573d78fa5b28b0
Adriano Avelino <adriano-st777@hotmail.com>
2020-04-06 18:03:32 -0300

    atualiza testes para pesquisa de jobs por key_word

--- spec/requests/api/v1/key_word_spec.rb
+++ spec/requests/api/v1/key_word_spec.rb
@@ -3,17 +3,27 @@ require "rails_helper"
 RSpec.describe "KeyWords", type: :request do
   let(:key_word) do
     create(:key_word).tap do |key_word|
-      create(:job_key_word, key_word: key_word)
+      create_list(:job_key_word, 3, key_word: key_word)
     end
   end
 
+  let(:key_word_attributes) { key_word.attributes }
+  let(:json_links) { %w[self first prev next last] }
   let(:parsed_response) { JSON.parse(response.body) }
   let(:parsed_json_attributes) do
     parsed_response["data"].first["attributes"].keys
   end
-  let(:json_attributes) { ["tag"] }
-  let(:job_attributes) { job.attributes }
-  let(:json_links) { %w[self first prev next last] }
+  let(:json_attributes) do
+    %w[
+      title
+      description
+      contract
+      job-link
+      salary
+      company-name
+      published-date
+    ]
+  end
 
   shared_examples "request endpoint" do
     it "responds with 200" do
@@ -26,21 +36,25 @@ RSpec.describe "KeyWords", type: :request do
     end
   end
 
-  shared_examples "pagination" do
-    it "shows links for pagination" do
-      expect(parsed_response["links"].keys).to match_array json_links
-    end
-  end
-
   describe "GET #index" do
-    subject(:index) { get api_v1_key_words_url }
+    subject(:index) { get api_v1_path(key_word.tag) }
 
     before do
       key_word
       index
     end
 
-    include_examples "request endpoint"
-    include_examples "pagination"
+    it "responds with 200" do
+      expect(response).to have_http_status :ok
+    end
+
+    it "responds with json" do
+      expect(parsed_json_attributes)
+        .to match_array json_attributes
+    end
+
+    it "shows links for pagination" do
+      expect(parsed_response["links"].keys).to match_array json_links
+    end
   end
 end
