9f0a3ab4d07009a3d280a8d1c9cae6a241749cd9
Adriano Avelino <adriano-st777@hotmail.com>
2020-04-07 11:02:35 -0300

    pesquisa de jobs por tag (#15)
    * adiciona teste para GET#index em key_words com response 200
    
    * adiciona rotes para key_words
    
    * adiciona controller para key_word
    
    * adiciona teste para GET#index em key_words com response with json
    
    * adiciona serializer para key_word
    
    * adiciona testes para GET#index com paginação
    
    * adiciona paginação no controller key_word
    
    * deleta o serializer de key_word
    
    * atualiza a rota para pesquisas por key_word
    
    * atualiza testes para pesquisa de jobs por key_word
    
    * adiciona a pesquisa de job por tag
    
    * remove a action show de KeyWordController
    
    * renomeia a variável
    
    * remove o shared example não utilizado
    
    * renomeia o teste de key_words para search_jobs
    
    * renomeia key_words_controller para search_jobs_controller
    
    * atualiza a rota após renomear controller
    
    * remove variável não usada

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :jobs, only: %i[index show]
+      get "search_jobs/:tag",  as: :search_jobs, to: "search_jobs#index"
     end
   end
 end
---
+++ spec/requests/api/v1/search_jobs_request_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+RSpec.describe "SearchJobs", type: :request do
+  let(:key_word) do
+    create(:key_word).tap do |key_word|
+      create_list(:job_key_word, 3, key_word: key_word)
+    end
+  end
+
+  let(:key_word_attributes) { key_word.attributes }
+  let(:json_links) { %w[self first prev next last] }
+  let(:parsed_response) { JSON.parse(response.body) }
+  let(:parsed_json_attributes) do
+    parsed_response["data"].first["attributes"].keys
+  end
+  let(:json_attributes) do
+    %w[
+      title
+      description
+      contract
+      job-link
+      salary
+      company-name
+      published-date
+    ]
+  end
+
+  describe "GET #index" do
+    subject(:index) { get api_v1_search_jobs_path(key_word.tag) }
+
+    before do
+      key_word
+      index
+    end
+
+    it "responds with 200" do
+      expect(response).to have_http_status :ok
+    end
+
+    it "responds with json" do
+      expect(parsed_json_attributes)
+        .to match_array json_attributes
+    end
+
+    it "shows links for pagination" do
+      expect(parsed_response["links"].keys).to match_array json_links
+    end
+  end
+end
